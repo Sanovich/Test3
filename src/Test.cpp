92291ec40f41fbc85dd3699d864c01d62a22abed
Администратор <@comp2112>
2019-10-10 22:28:19 +0300

    truck test

--- src/Test.cpp
+++ src/Test.cpp
@@ -2,15 +2,22 @@
 #include "ide_listener.h"
 #include "xml_listener.h"
 #include "cute_runner.h"
-
+#include "Truck.h"
+using namespace std;
 void thisIsATest() {
 	ASSERTM("start writing tests", false);	
 }
-
+void truckTest(){
+	Truck truck;
+	int expected = 1;
+	int actual = 1;
+	ASSERT_EQUAL(expected, actual);
+}
 bool runAllTests(int argc, char const *argv[]) {
 	cute::suite s { };
 	//TODO add your test here
 	s.push_back(CUTE(thisIsATest));
+	s.push_back(CUTE(truckTest));
 	cute::xml_file_opener xmlfile(argc, argv);
 	cute::xml_listener<cute::ide_listener<>> lis(xmlfile.out);
 	auto runner = cute::makeRunner(lis, argc, argv);
